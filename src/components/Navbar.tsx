51bf72e6f2cd2f7e495e60b0857ac811a20aa28c
TMB <mosesbakare48@gmail.com>
2026-09-13 20:18:27 +0100

    Increase Lateef logo display size in navbar header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -133,9 +133,9 @@ export default function Navbar() {
             <Image
               src="/assets/lateef-logo.png"
               alt="Lateeful Ul Akbar Il Aazam logo"
-              width={320}
-              height={96}
-              className={`h-11 w-auto min-w-0 object-contain sm:h-16 transition-all ${
+              width={400}
+              height={120}
+              className={`h-14 w-auto min-w-0 object-contain sm:h-20 md:h-24 transition-all ${
                 solid
                   ? "brightness-100 dark:brightness-0 dark:invert"
                   : "brightness-0 invert drop-shadow-[0_1px_10px_rgba(255,255,255,0.4)]"
